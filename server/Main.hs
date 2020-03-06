d3e4b95b18393259b3d7aed020679594a0feed12
maxfield-chen <maxfieldchen@gmail.com>
2020-03-05 20:12:05 -0800

    Implement Basic GET / POST Game functionality.

--- server/Main.hs
+++ server/Main.hs
@@ -1,4 +1,77 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
+import Prelude ()
+import Prelude.Compat
+
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Attoparsec.ByteString
+import Data.ByteString (ByteString)
+import Data.List
+import Data.Maybe
+import Data.String.Conversions
+import GHC.Generics
+import Lucid
+import Game
+import GameLogic
+import Proofs
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import System.Directory
+import Text.Blaze
+import Text.Blaze.Html.Renderer.Utf8
+import Servant.Types.SourceT (source)
+import qualified Data.Aeson.Parser
+import qualified Text.Blaze.Html
+
+-- Define JSON instances for Game data structures
+-- TODO: Decide if this should go back into the LGL package.
+instance ToJSON Game
+instance FromJSON Game
+instance ToJSON GameState
+instance FromJSON GameState
+instance ToJSON Group
+instance ToJSON MoveError
+instance ToJSON Outcome
+instance ToJSON Space
+instance FromJSON Space
+instance FromJSONKey Space
+instance ToJSONKey Space
+instance ToJSON (Game.Pair Int)
+instance FromJSON (Game.Pair Int)
+instance ToJSONKey (Game.Pair Int)
+instance FromJSONKey (Game.Pair Int)
+
+type GameAPI = "users" :> Get '[JSON] [Game]
+                :<|> "id" :> ReqBody '[JSON] Game :> Post '[JSON] Game
+
+server1 :: Server GameAPI
+server1 = exampleGame :<|> gameIdentity
+  where gameIdentity :: Game -> Handler Game
+        gameIdentity game = return game
+        exampleGame :: Handler [Game]
+        exampleGame = return [newGame]
+
+
+gameAPI :: Proxy GameAPI
+gameAPI = Proxy
+
+app1 :: Application
+app1 = serve gameAPI server1
+
 main :: IO ()
-main = print "test"
+main = run 9999 app1

8a13ad8d1d66c8a86377d45f41767f073432a0f0
maxfield-chen <maxfieldchen@gmail.com>
2020-04-18 08:35:28 -0700

    Add stylish haskell config, remove unused test files, add sample DB

--- lgs/tst/manual-calls/register_user.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST -d '{"userEmail": "max@gmail.com","userName": "max","userPassword": "test"}' -H 'Accept: application/json' -H 'Content-type: application/json' http://localhost:8888/users/register/ -vvv

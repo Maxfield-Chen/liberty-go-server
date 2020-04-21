b8a656bb095bc2ba842f4d3dedea07b074be73b1
maxfield-chen <maxfieldchen@gmail.com>
2020-04-21 07:32:51 -0700

    Add type definitions for API calls.

--- lgc/src/ServantClient.hs
+++ lgc/src/ServantClient.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE AllowAmbiguousTypes       #-}
+{-# LANGUAGE DataKinds                 #-}
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE GADTs                     #-}
 {-# LANGUAGE OverloadedStrings         #-}
@@ -13,18 +14,83 @@
 
 module ServantClient where
 
+import           Data.Map       (Map)
 import           Data.Proxy
-import qualified Data.Text      as Text
-import qualified GameDB         as GDB
+import           Data.Set       (Set)
+import           Data.Text
+import           Game           (Game, GameStatus, MoveError, Outcome, Position,
+                                 Space)
+import qualified GameDB
 import           LGSAPI
 import           Reflex
 import           Reflex.Dom
 import           Servant.API
 import           Servant.Reflex
 import qualified UserInput
+import           Web.Cookie
 
+
+url :: BaseUrl
+url = BaseFullUrl Http "localhost" 8888 ""
 
 apiClients :: forall t m. (MonadWidget t m) => _
-apiClients = client lgsAPI (Proxy :: Proxy m) (Proxy :: Proxy ()) (constDyn (BasePath "/"))
+apiClients = client lgsAPI (Proxy :: Proxy m) (Proxy :: Proxy ()) (constDyn url )
+
+proposeGame :: MonadWidget t m  =>
+               Dynamic t (Either Text UserInput.ProposedGame)
+               -> Event t ()
+               -> m (Event t (ReqResult () GameDB.GameRecord))
+
+acceptGameProposal :: MonadWidget t m =>
+                      Dynamic t (Either Text Int)
+                      -> Dynamic t (Either Text Bool)
+                      -> Event t ()
+                      -> m (Event t (ReqResult () (Maybe GameStatus)))
+
+pass :: MonadWidget t m =>
+              Dynamic t (Either Text Int)
+           -> Event t ()
+           -> m (Event t (ReqResult () (Maybe GameStatus)))
+
+proposeTerritory :: MonadWidget t m =>
+                          Dynamic t (Either Text Int)
+                          -> Dynamic t (Either Text (Map Space (Set Position)))
+                          -> Event t ()
+                          -> m (Event t (ReqResult () (Maybe GameStatus)))
+
+acceptTerritoryProposal :: MonadWidget t m =>
+                           Dynamic t (Either Text Int)
+                         -> Dynamic t (Either Text Bool)
+                         -> Event t ()
+                         -> m (Event t (ReqResult () (Maybe GameStatus)))
+
+placeStone :: MonadWidget t m =>
+                    Dynamic t (Either Text Int)
+                    -> Dynamic t (Either Text Position)
+                    -> Event t ()
+                    -> m (Event t (ReqResult () (Either MoveError Outcome, Game)))
+
+register :: MonadWidget t m =>
+                  Dynamic t (Either Text UserInput.User)
+                  -> Event t ()
+                  -> m (Event t (ReqResult () ()))
+
+login :: MonadWidget t m =>
+               Dynamic t (Either Text UserInput.Login)
+               -> Event t ()
+               -> m (Event t (ReqResult () (Headers
+                                            '[Header "Set-Cookie" SetCookie,
+                                              Header "Set-Cookie" SetCookie] NoContent)))
+
+gamesForUser :: MonadWidget t m =>
+                      Dynamic t (Either Text Int)
+                      -> Event t ()
+                      -> m (Event t (ReqResult () [GameDB.GameRecord]))
+
+getGame :: MonadWidget t m =>
+                 Dynamic t (Either Text Int)
+                 -> Event t ()
+                 -> m (Event t (ReqResult () (Maybe GameDB.GameRecord)))
+
 
 ((proposeGame :<|> acceptGameProposal :<|> pass :<|> proposeTerritory :<|> acceptTerritoryProposal :<|> placeStone) :<|> register :<|> login :<|> gamesForUser :<|> getGame) = apiClients

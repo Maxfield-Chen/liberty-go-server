2854a301063502078ede5d211ff0e566b9eabbc0
maxfield-chen <maxfieldchen@gmail.com>
2020-07-02 16:24:47 -0700

    Add styling for the propose game page.

--- lgc/src/ProposeGamePage.hs
+++ lgc/src/ProposeGamePage.hs
@@ -24,32 +24,44 @@ import qualified UserInput
 proposeGamePage :: forall t m. MonadWidget t m =>
              Dynamic t Page
           -> m (Event t ())
-proposeGamePage dynPage = elDynAttr "div" (shouldShow ProposeGame "register-page" <$> dynPage) $ do
-  text "Black Player"
-  dynMBlackPlayer :: Dynamic t (Maybe Int) <-
-    fmap (readMaybe . unpack) . value <$> inputElement def
-  text "White Player"
-  dynMWhitePlayer :: Dynamic t (Maybe Int) <-
-    fmap (readMaybe . unpack) . value <$> inputElement def
-  text "Black Teacher"
-  blackTeacher :: Dynamic t (Maybe Int) <-
-    fmap (readMaybe . unpack) . value <$> inputElement def
-  text "White Teacher"
-  whiteTeacher :: Dynamic t (Maybe Int) <-
-    fmap (readMaybe . unpack) . value <$> inputElement def
-  text "Black Focus"
-  blackFocus :: Dynamic t Text <-
-    value <$> inputElement def
-  text "White Focus"
-  whiteFocus :: Dynamic t Text <-
-    value <$> inputElement def
-  b <- button "Propose Game"
-  let userDyn = Right <$> (UserInput.ProposedGame <$>
-                          (fromMaybe (-1) <$> dynMBlackPlayer) <*>
-                          (fromMaybe (-1) <$> dynMWhitePlayer) <*>
-                          blackTeacher <*>
-                          whiteTeacher <*>
-                          blackFocus <*>
-                          whiteFocus)
-  proposeEv <- fmapMaybe reqSuccess <$> SC.proposeGame userDyn b
-  pure b
+proposeGamePage dynPage = elDynAttr "div" (shouldShow ProposeGame "register-page" <$> dynPage) $
+  divClass "form-prompt" $ do
+    dynMBlackPlayer :: Dynamic t (Maybe Int) <- divClass "propose-game" $ do
+      text "Black Player"
+      el "br" blank
+      fmap (readMaybe . unpack) . value <$> inputElement def
+    el "br" blank
+    dynMWhitePlayer :: Dynamic t (Maybe Int) <- divClass "propose-game-" $ do
+      text "White Player"
+      el "br" blank
+      fmap (readMaybe . unpack) . value <$> inputElement def
+    el "br" blank
+    blackTeacher :: Dynamic t (Maybe Int) <- divClass "propose-game-" $ do
+      text "Black Teacher"
+      el "br" blank
+      fmap (readMaybe . unpack) . value <$> inputElement def
+    el "br" blank
+    whiteTeacher :: Dynamic t (Maybe Int) <- divClass "propose-game-" $ do
+      text "White Teacher"
+      el "br" blank
+      fmap (readMaybe . unpack) . value <$> inputElement def
+    el "br" blank
+    blackFocus :: Dynamic t Text <- divClass "propose-game-" $ do
+      text "Black Focus"
+      el "br" blank
+      value <$> inputElement def
+    el "br" blank
+    whiteFocus :: Dynamic t Text <- divClass "propose-game-" $ do
+      text "White Focus"
+      el "br" blank
+      value <$> inputElement def
+    b <- divClass "submit-button" $ button "Propose Game"
+    let userDyn = Right <$> (UserInput.ProposedGame <$>
+                            (fromMaybe (-1) <$> dynMBlackPlayer) <*>
+                            (fromMaybe (-1) <$> dynMWhitePlayer) <*>
+                            blackTeacher <*>
+                            whiteTeacher <*>
+                            blackFocus <*>
+                            whiteFocus)
+    proposeEv <- fmapMaybe reqSuccess <$> SC.proposeGame userDyn b
+    pure b
